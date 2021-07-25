e30cffb078dff2246e6cf47b0b7afd6639a440f2
nicole <nicole.reyes.alveal@gmail.com>
2021-07-24 23:50:39 -0400

    navbar sin scroll

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
 
 const mobileMenu = () => {
     menu.classList.toggle('is-active')
@@ -7,3 +8,17 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu);
+
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home-page');
+    const aboutMenu = document.querySelector('#about-page');
+    const servicesMenu = document.querySelector('#services-page');
+    let scrollPos = window.scrollY
+
+    if (window.innerWidth > 960 && scrollPos < 600) {
+        homeMenu.classList.add('highlight')
+        aboutMenu.classList.remove('highlight')
+        return
+    }
+}
